ffa2e6f6112deff4985e5ad6a2374ca7a77b8b75
Rojan Thapa <rojan.thapa@konnectcraft.com>
2024-04-02 14:17:12 +0545

    feat: add scheduling in celery

--- proj/celery.py
+++ proj/celery.py
@@ -4,6 +4,11 @@ import os
 
 from celery import Celery
 
+from celery.schedules import crontab
+
+from datetime import timedelta
+
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj.settings')
 
 app = Celery('proj')
@@ -11,3 +16,17 @@ app = Celery('proj')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'run_transactions_every_30_seconds': {
+        'task': 'task3.tasks.run_transactions',
+        'schedule': timedelta(seconds=30),
+    },
+}
+
+# app.conf.beat_schedule = {
+#     'run_transactions_daily_at_3_15_pm': {
+#         'task': 'task3.tasks.run_transactions',
+#         'schedule': crontab(hour=15, minute=15),
+#     },
+# }
